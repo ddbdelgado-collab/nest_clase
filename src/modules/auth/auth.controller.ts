a6fc33463ec3169ee7884fd816cec40effd16604
ronni2030 <rav.villa@yavirac.edu.ec>
2026-06-15 08:36:08 -0500

    actualizacion 2

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterAuthDto } from './dto/register-auth.dto';
 
 @Controller('auth')
 export class AuthController {
     @Post('register')
-    registerUser(){
-
+    registerUser(@Body()userObj: RegisterAuthDto){
+        userObj.name
+        userObj.email
+        userObj.password
     }
 }
