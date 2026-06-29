88f0d4b607230cb850ee335b02c7072989725ebc
ronni2030 <rav.villa@yavirac.edu.ec>
2026-06-29 08:49:34 -0500

    tabla categoria y database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { databaseProvider } from './database.providers';
 import { ConfigService } from 'src/config/config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from 'src/config/config.module';
 
 @Module({
-    providers: [...databaseProvider, ConfigService],
-    exports: [...databaseProvider]
+  imports:[
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:(config: ConfigService)=>({
+
+        type:'postgres',
+        host:config.get('HOST'),
+        port: +config.get('PORT'),
+        username: config.get('USER'),
+        password: config.get('PASSWORD'),
+        database: config.get('DATABASE'),
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+      })
+    })
+  ],
+  providers:[...databaseProvider],
+  exports:[...databaseProvider]
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+
+}
